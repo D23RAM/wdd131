52f7a685218c9c2dd2c778b944b385fbf5b30315
D23RAM <dramoedit@byupathway.edu>
2026-07-30 20:19:17 -0300

    week5test1

---
+++ project-folder/script/form.js
@@ -0,0 +1,34 @@
+const products = [
+    {
+        id: "prod001",
+        name: "Smart Watch"
+    },
+    {
+        id: "prod002",
+        name: "Wireless Headphones"
+    },
+    {
+        id: "prod003",
+        name: "Laptop Stand"
+    },
+    {
+        id: "prod004",
+        name: "Bluetooth Speaker"
+    }
+];
+
+
+const productSelect = document.querySelector("#product");
+
+
+products.forEach(product => {
+
+    let option = document.createElement("option");
+
+    option.textContent = product.name;
+
+    option.value = product.id;
+
+    productSelect.appendChild(option);
+
+});
